4157afa99f50b324c2d974e93007937bfedff342
akyarv0 <akyarv0@gmail.com>
2024-04-24 20:50:24 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+// import Register from "./pages/Register";
+import AppRouter from "./router/AppRouter";
+
+const App = () => {
+  return (
+    <div className="dark:bg-gray-dark-main min-h-screen">
+   <AppRouter />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Main from "../pages/Main";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import MovieDetail from "../pages/MovieDetail";
+
+
+const AppRouter = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Main />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/register" element={<Register />}></Route>
+        <Route path="/detail/:id" element={<MovieDetail />}></Route>
+      </Routes>
+    </>
+  );
+};
+
+export default AppRouter;
